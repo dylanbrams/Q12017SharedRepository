7099e38d5ea909b79901a2b4f6d6e66b0b0e5bb4
Dylan Brams <dylan.brams@gmail.com>
2017-03-15 13:23:33 -0700

    Added account_test.py

---
+++ account.py
@@ -0,0 +1,13 @@
+class Account(object):
+    def __init__(self):
+        _interest = 0
+        _balance = 0
+
+
+#Main Definition
+def main():
+    return
+
+#Main Call
+if __name__ == '__main__':
+    main()
