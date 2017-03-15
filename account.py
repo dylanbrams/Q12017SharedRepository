1143b894c1737388e5f9ee6e0dd5c96f026d462d
Dylan Brams <dylan.brams@gmail.com>
2017-03-15 14:45:44 -0700

    Completed main, set up interface.

--- account.py
+++ account.py
@@ -1,3 +1,5 @@
+CONTINUE_PROCESSING_CHARACTERS = ['y', 'Y']
+
 class Account(object):
     def __init__(self):
         self._interest = 0.001
@@ -6,6 +8,35 @@ class Account(object):
 
 #Main Definition
 def main():
+    checking = Account()
+    savings = Account()
+    run_again_flag = True
+    while (run_again_flag == True):
+        checking_or_savings = input('Operate on Checking (1, default) or Savings (2)?')
+        if checking_or_savings == '2':
+            current_account = savings
+        else:
+            current_account = checking
+        print('Would you like to:')
+        print('1: Check Account Balance')
+        print('2: Deposit to your account')
+        print('3: Withdraw from your account')
+        print('4: Calculate interest on your account')
+        action = input("? >>> ")
+        if action == '2':
+            deposit = input ('How much are you depositing?')
+            current_account.deposit(deposit)
+        elif action == '3':
+            withdrawal = input ('How much are you withdrawing?')
+            amount_withdrawn = current_account.withdraw(withdrawal)
+            print ('Amount withdrawn: $' + str(amount_withdrawn))
+        elif action == '4':
+            interest = current_account.calc_interest()
+            print ('Interest Gained: ' + str(interest))
+        print ('Your Current Balance is : $' + str(current_account.get_funds()))
+        run_again = input('Run Again.')
+        if run_again.lower() not in CONTINUE_PROCESSING_CHARACTERS:
+            run_again_flag = False
     return
 
 #Main Call
